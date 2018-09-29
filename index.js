2ef3c34eee78535c4f753f57607d62c058f5db98
Eko Eryanto <ekoeryanto@gmail.com>
2018-09-29 21:19:58 +0700

    chore: add v1 plugin

--- index.js
+++ index.js
@@ -1,68 +1,38 @@
-#!/usr/bin/env node
-const { writeFile, existsSync } = require('fs')
-const { resolve } = require('path')
+const { path, fs, logger } = require('@vuepress/shared-utils')
 const { createSitemap } = require('sitemap')
-const esm = require('esm')
-const arg = require('arg')
 
-let args
+module.exports = ({ hostname, changefreq = 'daily', cacheTime = 600000, urls = [], ...others }, context) => ({
+  async generated () {
+    if (!hostname) {
+      return logger.warn(`\nNot generating sitemap because required 'hostname' option doesn't exist `)
+    }
 
-try {
-  args = arg({
-    'hostname': String,
-    'changefreq': String,
-    'cacheTime': String,
-    'dest': String,
+    logger.wait('\nGenerating sitemap...')
 
-    '-h': 'hostname',
-    '--hostname': 'hostname',
-    '-f': 'changefreq',
-    '--changefreq': 'changefreq',
-    '-c': 'cacheTime',
-    '--cache-time': 'cacheTime',
-    '-d': 'dest',
-    '--dest': 'dest'
-  })
-} catch (error) {
-  console.error(error.message)
-  process.exit(1)
-}
+    const { pages } = context.getSiteData()
+    const _urls = pages.map(i => {
+      const lastmodISO = i.lastUpdated ? new Date(i.lastUpdated).toISOString() : undefined
 
-if (!args.hostname) {
-  console.log('--hostname/-h is required')
-  process.exit(1)
-}
+      return {
+        url: i.path,
+        lastmodISO,
+        changefreq
+      }
+    })
+    .concat(urls)
 
-const requires = esm(module)
+    const sitemap = createSitemap({
+      hostname: hostname,
+      cacheTime: cacheTime,
+      urls: _urls,
+      ...others
+    })
 
-const tempData = resolve('node_modules/vuepress/lib/app/.temp/siteData')
+    const sitemapXML = path.resolve(context.outDir, 'sitemap.xml')
 
-const { siteData: { pages } } = requires(tempData)
-
-const urls = pages.map(i => {
-  const lastmodISO = i.lastUpdated ? new Date(i.lastUpdated).toISOString() : undefined
-
-  return {
-    url: i.path,
-    lastmodISO,
-    changefreq: args.changefreq || 'daily',
+    await fs.writeFile(
+      sitemapXML,
+      sitemap.toString()
+    )
   }
-})
-
-const sitemap = createSitemap({
-  hostname: args.hostname,
-  cacheTime: (args.cacheTime || 600) * 1000, // 600 sec cache period
-  urls,
-})
-
-const dest = resolve(args.dest || `${process.cwd()}/.vuepress/dist`)
-
-if (!existsSync(dest)) {
-  console.error('build dir does not exist:', dest)
-  process.exit(1)
-}
-
-writeFile(resolve(dest, 'sitemap.xml'), sitemap.toString(), (err) => {
-  if (!err) console.log(`sitemap generated in: "${dest}"`)
-  else console.error(err)
 })
