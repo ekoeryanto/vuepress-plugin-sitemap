1ce8790fe68abd47ec4b088bae8fc4f6ac136e1b
ekoeryanto <ekoeryanto@gmail.com>
2018-10-28 05:29:03 +0700

    chore: use context.pages and some tweak

--- index.js
+++ index.js
@@ -1,38 +1,53 @@
-const { path, fs, logger } = require('@vuepress/shared-utils')
-const { createSitemap } = require('sitemap')
+const fs = require("fs");
+const path = require("path");
+const { logger } = require("@vuepress/shared-utils");
+const { createSitemap } = require("sitemap");
 
-module.exports = ({ hostname, changefreq = 'daily', cacheTime = 600000, urls = [], ...others }, context) => ({
-  async generated () {
-    if (!hostname) {
-      return logger.warn(`\nNot generating sitemap because required 'hostname' option doesn't exist `)
-    }
+module.exports = (options, context) => {
+  const {
+    hostname,
+    outFile = "sitemap.xml",
+    changefreq = "daily",
+    cacheTime = 600000,
+    urls = [],
+    ...others
+  } = options;
 
-    logger.wait('\nGenerating sitemap...')
-
-    const { pages } = context.getSiteData()
-    const _urls = pages.map(i => {
-      const lastmodISO = i.lastUpdated ? new Date(i.lastUpdated).toISOString() : undefined
-
-      return {
-        url: i.path,
-        lastmodISO,
-        changefreq
+  return {
+    generated() {
+      if (!hostname) {
+        return logger.warn(
+          `\nNot generating sitemap because required 'hostname' option doesn't exist `
+        );
       }
-    })
-    .concat(urls)
 
-    const sitemap = createSitemap({
-      hostname: hostname,
-      cacheTime: cacheTime,
-      urls: _urls,
-      ...others
-    })
+      logger.wait("\nGenerating sitemap...");
 
-    const sitemapXML = path.resolve(context.outDir, 'sitemap.xml')
+      const { pages } = context;
+      const _urls = pages
+        .map(i => {
+          const lastmodISO = i.lastUpdated
+            ? new Date(i.lastUpdated).toISOString()
+            : undefined;
 
-    await fs.writeFile(
-      sitemapXML,
-      sitemap.toString()
-    )
-  }
-})
+          return {
+            url: i.path,
+            lastmodISO,
+            changefreq
+          };
+        })
+        .concat(urls);
+
+      const sitemap = createSitemap({
+        hostname: hostname,
+        cacheTime: cacheTime,
+        urls: _urls,
+        ...others
+      });
+
+      const sitemapXML = path.resolve(context.outDir, outFile);
+
+      fs.writeFileSync(sitemapXML, sitemap.toString());
+    }
+  };
+};
