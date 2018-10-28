4464c026dd764a4da77b74574ab638a033e15633
ekoeryanto <ekoeryanto@gmail.com>
2018-10-28 14:30:45 +0700

    feat(cli): custom temporary directory

--- cli.js
+++ cli.js
@@ -19,20 +19,25 @@ program
     val.split(",")
   )
   .option("-d, --dest <dest>", "vuepress dest dir")
+  .option(
+    "-t, --temp [temp]",
+    "vuepress temporary dir",
+    "node_modules/vuepress/lib/app/.temp/siteData"
+  )
   .parse(process.argv);
 
 if (!process.argv.slice(2).length) {
   program.outputHelp(chalk.green);
-  process.exit(-1)
+  process.exit(-1);
 }
 
 try {
-  const tempData = resolve("node_modules/vuepress/lib/app/.temp/siteData");
+  const tempData = resolve(program.temp);
   const requires = esm(module);
   const { siteData } = requires(tempData);
 
   make(program, siteData).generated();
 } catch (error) {
-  console.error(chalk.bold.red(error.message || error.msg || error))
-  process.exit(1)
+  console.error(chalk.bold.red(error.message || error.msg || error));
+  process.exit(1);
 }
