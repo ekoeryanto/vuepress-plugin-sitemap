42e0988495d1393be1f7aebead2903b60643376a
ekoeryanto <ekoeryanto@gmail.com>
2018-11-02 14:08:05 +0700

    feat: localization generation

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const fs = require("fs");
-const path = require("path");
-const chalk = require("chalk");
-const { createSitemap } = require("sitemap");
+const fs = require('fs');
+const path = require('path');
+const chalk = require('chalk');
+const { createSitemap } = require('sitemap');
 
-const log = (msg, color = "blue", label = "SITEMAP") =>
+const log = (msg, color = 'blue', label = 'SITEMAP') =>
   console.log(`\n${chalk.reset.inverse.bold[color](` ${label} `)} ${msg}`);
 
 module.exports = (options, context) => {
   const {
     hostname,
-    outFile = "sitemap.xml",
-    changefreq = "daily",
+    outFile = 'sitemap.xml',
+    changefreq = 'daily',
     cacheTime = 600,
     urls = [],
     ...others
@@ -21,37 +21,72 @@ module.exports = (options, context) => {
       if (!hostname) {
         return log(
           `Not generating sitemap because required 'hostname' option doesn't exist`,
-          "orange"
+          'orange'
         );
       }
 
-      log("Generating sitemap...");
+      log('Generating sitemap...');
 
-      const { pages } = context;
-      const _urls = pages
-        .map(i => {
-          const lastmodISO = i.lastUpdated
-            ? new Date(i.lastUpdated).toISOString()
-            : undefined;
+      const { pages, locales } = context;
 
-          return {
-            url: i.path,
-            lastmodISO,
-            changefreq
-          };
-        })
-        .concat(urls);
+      const localeKeys = locales && Object.keys(locales);
+
+      const pagesMap = new Map();
+
+      pages.forEach(page => {
+        const lastmodISO = page.lastUpdated
+          ? new Date(page.lastUpdated).toISOString()
+          : undefined;
+        pagesMap.set(page.path, { changefreq, lastmodISO });
+      });
+
+      if (localeKeys && localeKeys.length > 1) {
+        localeKeys.filter(x => x !== '/').forEach(locale => {
+          pagesMap.forEach((page, url) => {
+            if (!url.startsWith(locale)) return;
+
+            const parentURL = url.replace(locale, '/');
+            const parentPage = pagesMap.get(parentURL);
+            if (parentPage) {
+              if (!parentPage.links) {
+                parentPage.links = [
+                  {
+                    lang: locales['/'].lang,
+                    url: parentURL
+                  }
+                ];
+              }
+
+              parentPage.links.push({
+                lang: locales[locale].lang,
+                url
+              });
+            }
+
+            pagesMap.set(parentURL, parentPage);
+            pagesMap.delete(url);
+          });
+        });
+      }
 
       const sitemap = createSitemap({
         hostname: hostname,
         cacheTime: cacheTime * 1000,
-        urls: _urls,
         ...others
       });
 
+      pagesMap.forEach((page, url) => {
+        sitemap.add({ url, ...page });
+      });
+
+      // add custom urls
+      urls.forEach(url => sitemap.add(url));
+
+      log(`found ${sitemap.urls.length} locations`);
       const sitemapXML = path.resolve(context.outDir || options.dest, outFile);
 
       fs.writeFileSync(sitemapXML, sitemap.toString());
+      log(`${sitemap.urls.length} locations have been written.`);
     }
   };
 };
